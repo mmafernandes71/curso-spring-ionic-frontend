d4fdd64168db6d1f043201f1c53c0813aedb6d33
Nelio Alves <mmafernandes71@gmail.com>
2018-07-13 21:55:36 -0300

    Carregando produtos de uma dada categoria

---
+++ src/services/domain/produto.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_CONFIG } from '../../config/api.config';
+import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
+import { ProdutoDTO } from '../../models/produto.dto';
+
+@Injectable()
+export class ProdutoService {
+
+  constructor(public http: HttpClient) {
+  }
+
+  findByCategoria(categoria_id : string) {
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+  }
+
+}
