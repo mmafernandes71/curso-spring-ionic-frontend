550f7487f7971618d26ab53638a1f69a39ee6958
Nelio Alves <mmafernandes71@gmail.com>
2018-07-15 22:18:13 -0300

    Infinite scroll

--- src/pages/produto/produto.ts
+++ src/pages/produto/produto.ts
@@ -11,7 +11,8 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutoPage {
 
-  items: ProdutoDTO[];
+  items: ProdutoDTO[] = [];
+  page: number = 0;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -25,14 +26,16 @@ export class ProdutoPage {
 
   loadData() {
     let categoria_id = this.navParams.get('categoria_id');
-
     let loader = this.presentLoading();
-
-    this.produtoService.findByCategoria(categoria_id)
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
       .subscribe(response => {
-        this.items = response['content'];
+        let start = this.items.length;
+        this.items = this.items.concat(response['content']);
+        let end = this.items.length - 1;
         loader.dismiss();
-        this.loadImageUrls()
+        console.log(this.page);
+        console.log(this.items);
+        this.loadImageUrls(start, end);
       },
       error => {
         loader.dismiss();
@@ -40,8 +43,8 @@ export class ProdutoPage {
   }
    
 
-  loadImageUrls() {
-    for (var i=0; i < this.items.length; i++) {
+  loadImageUrls(start: number, end: number) {
+    for (var i=start; i<=end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
@@ -49,7 +52,7 @@ export class ProdutoPage {
         },
         error => {});
     }
-  }  
+  } 
 
   showDetail(produto_id: string) {
     this.navCtrl.push("ProdutoDetailPage", {produto_id: produto_id});
@@ -64,9 +67,19 @@ export class ProdutoPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
     setTimeout(() => {
       refresher.complete();
+    }, 1000);
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
     }, 1000);
   }
 
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -10,10 +10,11 @@ export class ProdutoService {
   constructor(public http: HttpClient) {
   }
 
-  findByCategoria(categoria_id : string) {
-    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+  findByCategoria(categoria_id : string, page : number = 0, linesPerPage : number = 24) {
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}&page=${page}&linesPerPage=${linesPerPage}`);
   }
 
+  
   getSmallImageFromBucket(id : string) : Observable<any> {
     let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
     return this.http.get(url, {responseType : 'blob'});
